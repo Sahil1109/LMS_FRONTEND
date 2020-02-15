a8f7515cc40db4db5e7668110197762754e8addc
RaviBlock8 <ravi83165@gmail.com>
2020-02-15 18:20:23 +0530

    improvements made in navbar and sidebar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -6,8 +6,11 @@ function Nav() {
             <div id="ham">
 
             </div>
+            <div id="notep">
+                <img src={require('../icons/notepad.png')} id="note_img" alt="User"></img> <span>LMS | EMPLOYEE</span>
+            </div>
             <div id="log">
-                <img src={require('../icons/user.png')} id="user_img" alt="User"></img>
+                <img src={require('../icons/userb.png')} id="user_img" alt="User"></img>
             </div>
         </div>
     )
--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -4,8 +4,8 @@ import '../styles/side.css'
 function SideBar() {
     return (
         <div id="sidebar">
-            <button type="button" key="2">Employee Leave</button>
-            <button type="button" key="3">Apply Leave</button>
+            <button type="button" id="dashb" key="2">DashBoard</button>
+            <button type="button" key="3" id="apply">Apply For Leave <img src={require('../icons/arrowb.png')} id="user_img" alt="User"></img></button>
         </div>
     )
 }
