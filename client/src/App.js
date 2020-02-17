7c2bd8cba8d27229ceb4d6952552df673de5a6eb
RaviBlock8 <ravi83165@gmail.com>
2020-02-17 13:10:40 +0530

    pushin akshay changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
+
 import Nav from './components/Nav'
 import Sidebar from './components/SideBar'
-import LeaveTable from './components/LeaveTable'
-import LeaveList from './components/LeaveList'
 import EmpDashboard from './components/EmpDashboard'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App" id="app">
+      {/* this will contain only our navbar */}
       <div id="top">
       <Nav></Nav>
       </div>
 
       
       <div id="main">
+      {/* this will also stay same in every dashboard except the botton component will change */}
       <Sidebar></Sidebar>
       {/* your application div should be placed inside a div like this */}
       <EmpDashboard></EmpDashboard>
