df3ea37c6b902f4ad74457cf4076d461f45a12fa
RaviBlock8 <ravi83165@gmail.com>
2020-02-18 11:45:38 +0530

    adding logout button

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,13 +4,13 @@ function Nav() {
     return (
         <div id="nav">
             <div id="ham">
-
+            <img src={require('../icons/block8.png')} id="logo_img" alt="User"></img>
             </div>
             <div id="notep">
                 <img src={require('../icons/notepad.png')} id="note_img" alt="User"></img> <span>LMS | EMPLOYEE</span>
             </div>
             <div id="log">
-                <img src={require('../icons/userb.png')} id="user_img" alt="User"></img>
+                <img src={require('../icons/userb.png')} id="user_img" alt="User"></img> <span id="sep">|</span> <a href="#"><img src={require('../icons/logout.png')} id="logout_img" alt="User"></img></a> 
             </div>
         </div>
     )
