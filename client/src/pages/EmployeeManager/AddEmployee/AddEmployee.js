a0a0ce54104ead64df3619a07abdef472e2383e0
Sahil1109 <sahil.jangra@block8.com>
2020-03-16 09:14:35 +0530

    Employee manager section updated

--- client/src/pages/EmployeeManager/AddEmployee/AddEmployee.js
+++ client/src/pages/EmployeeManager/AddEmployee/AddEmployee.js
@@ -1,140 +1,524 @@
-import React from "react";
-// import "./AddEmployee.css";
-import './addemp.css'
-import DatePicker from "react-date-picker";
-import { useFormState } from "react-use-form-state";
-
-
-
-
-export default function AddEmployee({ onSubmit }) {
-  const [formState, { text, email, password, select }] = useFormState({
-    role: "N/A",
-    status: "N/A",
-    gender: "N/A"
-  });
-
-  function handleSubmit(e) {
-    // ..
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import "./AddEmployee.css";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import MomentUtils from "@date-io/moment";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker
+} from "@material-ui/pickers";
+import MuiAlert from "@material-ui/lab/Alert";
+import { Snackbar } from "@material-ui/core/";
+const genders = [
+  {
+    value: "male",
+    label: "Male"
+  },
+  {
+    value: "female",
+    label: "Female"
   }
+];
+
+const statuses = [
+  {
+    value: "active",
+    label: "Active"
+  },
+  {
+    value: "inactive",
+    label: "Inactive"
+  }
+];
+const streams = [
+  {
+    value: "a",
+    label: "A"
+  },
+  {
+    value: "b",
+    label: "B"
+  }
+];
+const roles = [
+  {
+    value: "admin",
+    label: "Admin"
+  },
+  {
+    value: "employee",
+    label: "Employee"
+  }
+];
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(0.9),
+      width: "100%",
+      padding: theme.spacing(0.9),
+      flexGrow: 1
+    }
+  },
+  fieldWidth: {
+    width: "100%"
+  },
+  formControl: {
+    margin: theme.spacing(0.9),
+    minWidth: 140
+  }
+}));
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const AddEmployee = () => {
+  const classes = useStyles();
+  const { register, handleSubmit } = useForm();
+  const [Snackopen, setSnackOpen] = useState(false);
+  const [warningOpen, setWarningOpen] = useState(false);
+  const [warningMsg, setWarningMsg] = useState("");
+  const [status, setStatus] = useState("active");
+  const [role, setRole] = useState("employee");
+  const [gender, setGender] = useState("male");
+  const [stream, setStream] = useState("a");
+  let [doj, setDoj] = useState(new Date());
+  let [dob, setDob] = useState(new Date());
+  let [doa, setDoa] = useState(new Date());
+
+  const url = "http://localhost:5000/employee";
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get(url).then(json => setData(json.data));
+  }, []);
+
+  const [approver, setApprover] = useState([data.firstName]);
+  const handleStatusChange = event => {
+    setStatus(event.target.value);
+  };
+  const handleRoleChange = event => {
+    setRole(event.target.value);
+  };
+  const handleGenderChange = event => {
+    setGender(event.target.value);
+  };
+  const handleStreamChange = event => {
+    setStream(event.target.value);
+  };
+  const handleDobChange = event => {
+    setDob(event);
+  };
+  const handleDojChange = event => {
+    setDoj(event);
+  };
+  const handleDoaChange = event => {
+    setDoa(event);
+  };
+  const handleApproverChange = event => {
+    setApprover(event.target.value);
+    console.log(approver);
+  };
+  const handleSnackClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnackOpen(false);
+  };
+  const history = useHistory();
+
+  const loadAnotherPage = page => {
+    switch (page) {
+      case "employeeManager":
+        history.push("/employeeManager");
+        break;
+      default:
+        history.push("/");
+        break;
+    }
+  };
+  const onSubmit = data => {
+    let obj = {
+      firstName: data.firstName,
+      middleName: data.middleName,
+      lastName: data.lastName,
+      email: data.email,
+      empId: Number(data.empId),
+      doj: doj.toISOString(),
+      role: role.toLowerCase(),
+      approver: data.approver,
+      status: status.toLowerCase(),
+      gender: gender.toLowerCase(),
+      password: data.Password
+      // doa: "doa.toISOString()",
+      // stream: stream.toLowerCase(),
+      // dob: dob.toISOString(),
+      // mobileNo: data.mobileNo,
+      // emergencyName: data.emergencyName,
+      // emergencyContact: data.emergencyContact,
+      // confirmPassword: data.confirmPassword
+    };
+    // if (obj.password !== obj.confirmPassword) {
+    //   console.log("password error");
+    //   setWarningMsg("Password must be same");
+    //   setWarningOpen(true);
+    // }
+    // if (obj.password === obj.confirmPassword) {
+    //   console.log("password same");
+    //   setSnackOpen(true);
+    // }
+    console.log(obj);
+    // axios
+    //   .post("http://localhost:5000/employee", obj)
+    //   .then(res => {
+    //     console.log("Employee added");
+    //     setSnackOpen(true);
+    //   })
+    //   .catch((err, data) => {
+    //     setWarningMsg("Error while adding employee");
+    //     setWarningOpen(true);
+    //     console.log("error while adding");
+    //     console.log(err.response);
+    //   });
+  }; // your form submit function which will invoke after successful validation
+
   return (
     <div>
-      <form onSubmit={handleSubmit} id="addempform">
-        <div id="name-group">
-          <div className="field-group">
-            <label>
-              First name<span id="mandatory">*</span>
-            </label>
-            <input
-              {...text({
-                name: "firstName",
-                onChange: e => console.log("first name changes")
+      <Grid container xs={12}>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={classes.root}
+          noValidate
+          autoComplete="off"
+          id="addempform"
+        >
+          <Grid item xs={3}>
+            <TextField
+              id="filled-error-helper-text"
+              inputRef={register({
+                required: true,
+                minLenth: 3,
+                maxLength: 50
               })}
+              label="First name"
+              variant="outlined"
+              name="firstName"
               required
-              minLength="4"
+              className={classes.fieldWidth}
             />
-          </div>
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
+              id="outlined-basic"
+              inputRef={register({ maxLength: 50 })}
+              label="Middle name"
+              variant="outlined"
+              name="middleName"
+            />
+          </Grid>
 
-          <div className="field-group">
-            <label>Middle name</label>
-            <input {...text("middleName")} />
-          </div>
+          <Grid item xs={3}>
+            <TextField
+              id="outlined-basic"
+              inputRef={register({
+                required: true,
+                minLenth: 3,
+                maxLength: 50
+              })}
+              label="Last name"
+              variant="outlined"
+              name="lastName"
+              required
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              id="outlined-basic"
+              inputRef={register({
+                required: true,
+                maxLength: 50
+              })}
+              label="Email"
+              variant="outlined"
+              name="email"
+              type="email"
+              required
+              style={{ width: "300px" }}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <TextField
+              id="outlined-select"
+              select
+              required="true"
+              label="Gender"
+              value={gender}
+              onChange={handleGenderChange}
+              variant="outlined"
+            >
+              {genders.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={2} className="date-padding">
+            <MuiPickersUtilsProvider
+              utils={MomentUtils}
+              className="date-padding"
+            >
+              <KeyboardDatePicker
+                required
+                margin="normal"
+                id="dob"
+                label="Date of birth"
+                format="DD/MM/YYYY"
+                value={dob}
+                style={{ paddingTop: 0 }}
+                onChange={handleDobChange}
+                KeyboardButtonProps={{
+                  "aria-label": "change date"
+                }}
+              />
+            </MuiPickersUtilsProvider>
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
+              id="outlined-basic"
+              inputRef={register({ maxLength: 10 })}
+              label="Mobile no."
+              variant="outlined"
+              name="mobileNo"
+              type="number"
+            />
+          </Grid>
+          <hr style={{ padding: 0 }}></hr>
+          <Grid item xs={2}>
+            <TextField
+              id="outlined-required"
+              inputRef={register({ required: true, maxLength: 3 })}
+              label="Employee id"
+              variant="outlined"
+              name="empId"
+              type="number"
+              required
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+              <KeyboardDatePicker
+                margin="normal"
+                required
+                id="dob"
+                label="Date of joining"
+                format="DD/MM/YYYY"
+                value={doj}
+                onChange={handleDojChange}
+                KeyboardButtonProps={{
+                  "aria-label": "change date"
+                }}
+              />
+            </MuiPickersUtilsProvider>
+          </Grid>
+          <Grid item xs={4}>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+              <KeyboardDatePicker
+                disabled
+                style={{ padding: 0 }}
+                margin="normal"
+                id="dob"
+                label="Date of leaving"
+                format="DD/MM/YYYY"
+                value={doa}
+                onChange={handleDoaChange}
+                KeyboardButtonProps={{
+                  "aria-label": "change date"
+                }}
+              />
+            </MuiPickersUtilsProvider>
+          </Grid>
+          <Grid item xs={2}>
+            <TextField
+              id="outlined-select"
+              select
+              required="true"
+              label="Status"
+              value={status}
+              onChange={handleStatusChange}
+              variant="outlined"
+            >
+              {statuses.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
+              id="outlined-select"
+              select
+              required="true"
+              label="Stream"
+              value={stream}
+              onChange={handleStreamChange}
+              variant="outlined"
+              style={{ width: "80px" }}
+            >
+              {streams.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={3}>
+            {/* <TextField
+              id="outlined-basic"
+              inputRef={register({ maxLength: 50 })}
+              label="Approver"
+              variant="outlined"
+              name="approver"
+            /> */}
+            <TextField
+              id="outlined-select"
+              select
+              required="true"
+              label="Approver"
+              value={approver}
+              onChange={handleApproverChange}
+              variant="outlined"
+              style={{ width: "150px"}}
+            >
+              {data.map(option => (
+                <MenuItem key={option.id} value={option.id}>
+                  {option.firstName}
+                </MenuItem>
+              ))}
+            </TextField>
 
-          <div className="field-group">
-            <label>
-              Last name <span id="mandatory">*</span>
-            </label>
-            <input {...text("lastName")} required />
-          </div>
-        </div>
-        <div className="group">
-          <div className="field-group" >
-            <label>
-              Email<span id="mandatory">*</span>
-            </label>
-            <input {...email("email")} required />
-          </div>
-          <div className="field-group">
-            <label>
-              Employee Id<span id="mandatory">*</span>
-            </label>
-            <input {...text("empId")} required />
-          </div>
-        </div>
-        <div className="group">
-          <div className="field-group">
-            <label>
-              Status<span id="mandatory">*</span>
-            </label>
-            <select {...select("status")} required>
-              <option value="N/A">Choose type</option>
-              <option value="active">Active</option>
-              <option value="inactive">Inactive</option>
-            </select>
-          </div>
-          <div className="field-group date-group">
-            <label>
-              Date of joining<span id="mandatory">*</span>
-            </label>
-            <DatePicker />
-          </div>
-        </div>
-        <div id="role-group">
-          <div className="field-group">
-            <label>
-              Role<span id="mandatory">*</span>
-            </label>
-            <select {...select("role")} required>
-              <option value="N/A">Choose type</option>
-              <option value="admin">Admin</option>
-              <option value="employee">Employee</option>
-            </select>
-          </div>
+          </Grid>
 
-          <div className="field-group">
-            <label>
-              Approver<span id="mandatory">*</span>
-            </label>
-            <input {...text("approver")} required />
-          </div>
+          <Grid item xs={3}>
+            <TextField
+              id="outlined-select"
+              select
+              required="true"
+              label="Role"
+              value={role}
+              onChange={handleRoleChange}
+              variant="outlined"
+            >
+              {roles.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <hr style={{ padding: 0 }}></hr>
+          <Grid item xs={3}>
+            <TextField
+              id="outlined-basic"
+              inputRef={register({ maxLength: 50 })}
+              label="Emergency contact name"
+              variant="outlined"
+              name="emergencyName"
+            />
+          </Grid>
 
-          <div className="field-group">
-            <label>
-              Gender<span id="mandatory">*</span>
-            </label>
-            <select {...select("gender")} required>
-              <option value="N/A">Choose type</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-            </select>
-          </div>
-        </div>
-        <div className="group">
-          <div className="field-group">
-            <label>
-              Password<span id="mandatory">*</span>
-            </label>
-            <input {...password("passowrd")} required />
-          </div>
-          <div className="field-group">
-            <label>
-              Confirm password<span id="mandatory">*</span>
-            </label>
-            <input {...password("confirmPassowrd")} required />
-          </div>
-        </div>
-        <div id="button-group">
-        
-          <button id="saveBtn" type="submit">
-            Save
-          </button>
-
-          <button id="cancelBtn" type="submit">
-            Cancel
-          </button>
-         
-        </div>
-      </form>
+          <Grid item xs={3}>
+            <TextField
+              id="outlined-basic"
+              inputRef={register({ maxLength: 10 })}
+              label="Emergency contact no."
+              variant="outlined"
+              name="emergencyContact"
+              type="number"
+            />
+          </Grid>
+          <Grid item xs={5}></Grid>
+          <Grid item xs={3}>
+            <TextField
+              required
+              id="outlined-required"
+              inputRef={register({ required: true, maxLength: 50 })}
+              label="Password"
+              variant="outlined"
+              name="Password"
+              type="password"
+            />
+          </Grid>
+          <Grid item xs={3}>
+            <TextField
+              required
+              id="outlined-required"
+              inputRef={register({ required: true, maxLength: 50 })}
+              label="Confirm password"
+              variant="outlined"
+              name="confirmPassword"
+              type="password"
+            />
+          </Grid>
+          <Grid item xs={1}>
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              id="saveButton"
+              style={{ color: "white" }}
+            >
+              Submit
+            </Button>
+          </Grid>
+          <Grid item xs={1}>
+            <Button
+              variant="contained"
+              color="secondary"
+              type="submit"
+              id="cancelButton"
+              onClick={() => {
+                loadAnotherPage("employeeManager");
+              }}
+            >
+              Cancel
+            </Button>
+          </Grid>
+        </form>
+      </Grid>
+      <Snackbar
+        open={Snackopen}
+        autoHideDuration={1500}
+        onClose={handleSnackClose}
+      >
+        <Alert onClose={handleSnackClose} severity="success">
+          Employee Added
+        </Alert>
+      </Snackbar>
+      <Snackbar
+        open={warningOpen}
+        autoHideDuration={1500}
+        onClose={() => {
+          setWarningOpen(false);
+        }}
+      >
+        <Alert
+          onClose={() => {
+            setWarningOpen(false);
+          }}
+          severity="error"
+        >
+          {warningMsg}
+        </Alert>
+      </Snackbar>
     </div>
   );
-}
+};
+
+export default AddEmployee;
--- client/src/pages/EmployeeManager/EmployeeManagerTable.js
+++ client/src/pages/EmployeeManager/EmployeeManagerTable.js
@@ -1,24 +1,108 @@
-import React from "react";
-// import "./EmployeeManager.css";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import "./EmployeeManager.css";
+import axios from "axios";
+// import RootURL from "../../handlers/RootUrl";
+import Button from "@material-ui/core/Button";
+import { Link } from "@material-ui/core/";
+import EditIcon from "@material-ui/icons/Edit";
+import getStringDate from "../../handlers/StringData";
 
-function EmployeeManagerTable() {
+export default function EmployeeManagerTable() {
+  const url = "http://localhost:5000/employee";
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then(json => setData(json.data));
+  }, []);
+
+  const renderTable = () => {
+    return data.map(user => {
+      return (
+        <tr>
+          <td>{user.empId}</td>
+          <td>
+            {user.firstName} {user.middleName} {user.lastName}
+          </td>
+          <td>{getStringDate(user.doj)}</td>
+          <td>{user.role}</td>
+          <td>{user.email}</td>
+          <td>
+            {
+              <Link style={{ cursor: "pointer" }}>
+                <EditIcon />
+              </Link>
+            }
+          </td>
+        </tr>
+      );
+    });
+  };
+  const history = useHistory();
+
+  const loadAnotherPage = page => {
+    switch (page) {
+      case "addEmployee":
+        history.push("/addEmployee");
+        break;
+      case "editEmployee":
+        history.push("/editEmployee");
+        break;
+      default:
+        history.push("/");
+        break;
+    }
+  };
+
   return (
     <div>
-      <button id="addEmp">ADD EMPLOYEE</button>
+      <Grid container>
+        <Grid item={1}>
+          <Button
+            variant="contained"
+            color="primary"
+            type="button"
+            id="saveButton"
+            style={{ color: "white", marginTop: "5px", marginLeft: "5px" }}
+            onClick={() => {
+              loadAnotherPage("addEmployee");
+            }}
+          >
+            ADD EMPLOYEE
+          </Button>
+        </Grid>
+        <Grid item={1}>
+          <Button
+            variant="contained"
+            color="primary"
+            type="button"
+            id="saveButton"
+            style={{ color: "white", marginTop: "5px", marginLeft: "5px"}}
+            onClick={() => {
+              loadAnotherPage("editEmployee");
+            }}
+          >
+            EDIT EMPLOYEE
+          </Button>
+        </Grid>
+      </Grid>
       <div id="tableEmp">
         <table>
-          <tr id="thrEmp">
-            <th>Employee ID</th>
-            <th>Employee name</th>
-            <th>Date of joining</th>
-            <th>Role</th>
-            <th>Email</th>
-            <th>Edit</th>
-          </tr>
+          <thead>
+            <tr id="thrEmp">
+              <th>Employee ID</th>
+              <th>Employee name</th>
+              <th>Date of joining</th>
+              <th>Role</th>
+              <th>Email</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>{renderTable()}</tbody>
         </table>
       </div>
     </div>
   );
 }
-
-export default EmployeeManagerTable;
